bc7d8c8b26dcd7acff4c1436cb1a3aa73c01b5c0
Alif Taufik <alif.tr.aminuddin@gmail.com>
2019-11-14 18:13:22 +0700

    start creating roomlist tables

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,20 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <RoomList></RoomList>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import RoomList from "@/components/RoomList";
 
 export default {
-  name: 'home',
+  name: "home",
   components: {
-    HelloWorld
+    RoomList
+  },
+  data() {
+    return {};
   }
-}
+};
 </script>
