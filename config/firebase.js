db1e40f0fee23b5b9bac053702b9b19276486ab7
nadhiljanitra <nadhiljanitraa@gmail.com>
2019-11-14 17:12:44 +0700

    skeletonClient

---
+++ config/firebase.js
@@ -0,0 +1,17 @@
+const firebase = require('firebase/app')
+require('firebase/firestore')
+
+
+firebase.initializeApp({
+  apiKey: process.env.VUE_APP_apiKey,
+  authDomain: process.env.VUE_APP_authDomain,
+  databaseURL: process.env.VUE_APP_databaseURL,
+  projectId: process.env.VUE_APP_projectId,
+  storageBucket: process.env.VUE_APP_storageBucket,
+  messagingSenderId: process.env.VUE_APP_messagingSenderId,
+  appId: process.env.VUE_APP_appId,
+})
+
+var db = firebase.firestore()
+
+module.exports = db
