1e482e261c41b089572d12c4eb525a7ba690daa0
Alif Taufik <alif.tr.aminuddin@gmail.com>
2019-11-14 19:38:37 +0700

    creating roomlist view

--- src/components/RoomList.vue
+++ src/components/RoomList.vue
@@ -1,55 +1,46 @@
 <template>
   <b-table
-    :striped="striped"
-    :bordered="bordered"
-    :borderless="borderless"
-    :outlined="outlined"
-    :small="small"
-    :hover="hover"
-    :dark="dark"
-    :fixed="fixed"
-    :foot-clone="footClone"
-    :no-border-collapse="noCollapse"
-    :items="items"
+    show-empty
+    :striped="true"
+    :bordered="true"
+    :borderless="true"
+    :small="true"
+    :hover="true"
+    :fixed="true"
     :fields="fields"
-    :head-variant="headVariant"
-    :table-variant="tableVariant"
-  ></b-table>
+    :items="roomList"
+    head-variant="dark"
+  >
+    <template v-slot:cell(no)="row">{{ row.value }} {{ row.value }}</template>
+
+    <template v-slot:cell(join)="row">
+      <b-button size="sm" @click="join(row.item, row.index, $event.target)" class="mr-1">Join Room</b-button>
+    </template>
+  </b-table>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      fields: ["first_name", "last_name", "age"],
-      items: [
-        { age: 40, first_name: "Dickerson", last_name: "Macdonald" },
-        { age: 21, first_name: "Larsen", last_name: "Shaw" },
-        { age: 89, first_name: "Geneva", last_name: "Wilson" }
-      ],
-      tableVariants: [
-        "primary",
-        "secondary",
-        "info",
-        "danger",
-        "warning",
-        "success",
-        "light",
-        "dark"
-      ],
-      striped: false,
-      bordered: false,
-      borderless: false,
-      outlined: false,
-      small: false,
-      hover: false,
-      dark: false,
-      fixed: false,
-      footClone: false,
-      headVariant: null,
-      tableVariant: "",
-      noCollapse: false
+      fields: [{ key: "id", label: "No." }, "room_name", "join"]
     };
+  },
+  methods: {
+    join(item, index, button) {
+      // Update data room yang ada di db, id room ada di item.id
+      // Pindah masuk ke dalem room (belum kebayang caranya)
+      // console.log("item:", item);
+      // console.log("item id:", item.id);
+      // console.log("index:", index);
+      // console.log("button:", button);
+    }
+  },
+  computed: {
+    roomList() {
+      // Dapetin semua data room yang ada
+      return this.$store.state.room_list;
+    }
   }
 };
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,27 @@
 <template>
   <div>
-    <RoomList></RoomList>
+    <b-jumbotron bg-variant="primary" text-variant="white" fluid>
+      <template slot="header">Room List</template>
+      <template slot="lead">Pick a room or create new one!</template>
+    </b-jumbotron>
+    <div class="container">
+      <RoomList></RoomList>
+      <b-form inline @submit.prevent="onSubmit">
+        <b-form-group label="Create New Room:" label-for="room_name">
+          <b-input-group class="mx-3" prepend="@">
+            <b-form-input
+              prepend="@"
+              id="room_name"
+              v-model="room_name"
+              required
+              placeholder="swalisdangerous"
+            ></b-form-input>
+          </b-input-group>
+        </b-form-group>
+
+        <b-button type="submit" variant="primary">Submit</b-button>
+      </b-form>
+    </div>
   </div>
 </template>
 
@@ -14,7 +35,16 @@ export default {
     RoomList
   },
   data() {
-    return {};
+    return {
+      room_name: ""
+    };
+  },
+  methods: {
+    onSubmit() {
+      this.$store.commit("addRoom", this.room_name); // Commit untuk nambah room baru. Harusnya dispatch
+      // this.$router.push("/gameroom"); Ketika bikin room langsung masuk ke room
+    },
+    onReset() {}
   }
 };
 </script>
